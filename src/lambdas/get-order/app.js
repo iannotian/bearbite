36502b3d643acebf197868b40a776764ff148901
Ian MacFarlane <1900909+iannotian@users.noreply.github.com>
2022-03-06 23:01:27 -0500

    Add notes to lambdas

--- src/lambdas/get-order/app.js
+++ src/lambdas/get-order/app.js
@@ -1,18 +1,7 @@
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
-
 exports.lambdaHandler = async (event, context) => {
   try {
+    // @todo Check order status for ID (or API key) in DynamoDB and return it.
+
     console.info("Someday this will get an order!");
   } catch (error) {
     console.error("Error!");
--- src/lambdas/post-order/app.js
+++ src/lambdas/post-order/app.js
@@ -1,18 +1,10 @@
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
-
 exports.lambdaHandler = async (event, context) => {
   try {
+    // @todo Check if order was placed for user in DynamoDB
+    // @todo Inform user if order pending, completed or errored for the day. Guard return order ID.
+    // @todo Place order request on queue for process-order Lambda.
+    // @todo Return order ID.
+
     console.info("Someday this will post an order!");
   } catch (error) {
     console.error("Error!");
